6c07b56bf8e7690ca5c135e300a51087a904a79a
Jake Schoermer <jake@secretservers.net>
2012-10-29 11:53:53 +1000

    Renames Files based on Student Number

--- get_essays.sh
+++ get_essays.sh
@@ -1,5 +1,9 @@
+ cd "downloads"
  while IFS=, read col1 col2 remainder
  do	
- 	cd "downloads"
-	wget "$col2"
- done < "submissions.csv"
+ 	
+	ext=`echo $col2 | awk -F"." '{print $6}'`
+	fname="$col1.$ext"
+	
+	wget  --output-document "$fname" "$col2"
+ done < "../submissions.csv"
